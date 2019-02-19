34d146cb84df70e2b8ebf412ac11a538bdf87391
Monroe Williams <monroe@pobox.com>
2019-02-19 05:09:51 -0800

    Update ABS preheat bed temp to 85.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1265,7 +1265,7 @@
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 250
-#define PREHEAT_2_TEMP_BED    90
+#define PREHEAT_2_TEMP_BED    85
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
