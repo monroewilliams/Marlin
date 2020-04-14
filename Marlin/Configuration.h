9460ec17c5f63e7b35ad154814992fa34318ec57
Monroe Williams <monroe@pobox.com>
2020-04-13 23:14:46 -0700

    Double all steps/unit numbers. The steppers I'm using seem to have twice the resolution of the Melzi ones.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -802,7 +802,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 94 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 188 }
 
 /**
  * Default Max Feed Rate (mm/s)
