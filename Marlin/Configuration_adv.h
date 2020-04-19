09c2002cd1ac433cef60d10ef51b7d19b6f036e7
Monroe Williams <monroe@pobox.com>
2020-04-19 16:21:19 -0700

    Switch to unified bed levelling

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1529,12 +1529,12 @@
  * Override if the automatically selected points are inadequate.
  */
 #if EITHER(AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_UBL)
-  #define PROBE_PT_1_X 200
-  #define PROBE_PT_1_Y 0
-  #define PROBE_PT_2_X 0
-  #define PROBE_PT_2_Y 100
-  #define PROBE_PT_3_X 200
-  #define PROBE_PT_3_Y 200
+  //#define PROBE_PT_1_X 15
+  //#define PROBE_PT_1_Y 180
+  //#define PROBE_PT_2_X 15
+  //#define PROBE_PT_2_Y 20
+  //#define PROBE_PT_3_X 170
+  //#define PROBE_PT_3_Y 20
 #endif
 
 /**
