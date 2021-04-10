662c3a73a3cb875573f8e0950ed9725c040e6323
Monroe Williams <monroe@pobox.com>
2021-04-09 18:53:59 -0700

    Adjust for new X endstop and correct Z max.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1234,12 +1234,12 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -20
+#define X_MIN_POS -17
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 236
+#define X_MAX_POS 234
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 210
+#define Z_MAX_POS 275
 
 /**
  * Software Endstops
