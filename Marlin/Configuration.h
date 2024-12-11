bc32d9a5ff1169a5bc186ceb0a1c78af26d3d2a5
Monroe Williams <monroe@pobox.com>
2024-12-10 21:07:08 -0800

    Updates for larger MK52 bed size.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1259,14 +1259,14 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 250
+#define Y_BED_SIZE 215
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -7
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 249
+#define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -809,7 +809,8 @@
   // Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
   // If not defined, probe limits will be used.
   // Override with 'M422 S<index> X<pos> Y<pos>'
-  #define Z_STEPPER_ALIGN_XY { {  10, 110 }, { 210,  110 }}
+  // These will be offset using NOZZLE_TO_PROBE_OFFSET to position the carriage for the probes
+  #define Z_STEPPER_ALIGN_XY { {  20, Y_CENTER }, { 235,  Y_CENTER }}
 
   /**
    * Orientation for the automatically-calculated probe positions.
@@ -1825,7 +1826,7 @@
  * the probe to be unable to reach any points.
  */
 #if PROBE_SELECTED && !IS_KINEMATIC
-  //#define PROBING_MARGIN_LEFT PROBING_MARGIN
+  #define PROBING_MARGIN_LEFT 20
   //#define PROBING_MARGIN_RIGHT PROBING_MARGIN
   //#define PROBING_MARGIN_FRONT PROBING_MARGIN
   //#define PROBING_MARGIN_BACK PROBING_MARGIN
