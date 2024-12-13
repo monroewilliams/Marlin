1837db4dd6ec0a0c4c34e92ebb42e7f6bb1d208a
Monroe Williams <monroe@pobox.com>
2024-12-12 19:18:24 -0800

    Increase the allowed bed temp variation from 2 degrees to 5 degrees to avoid spurious thermal runaway detections

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -250,13 +250,13 @@
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
   #define THERMAL_PROTECTION_BED_PERIOD        20 // Seconds
-  #define THERMAL_PROTECTION_BED_HYSTERESIS     2 // Degrees Celsius
+  #define THERMAL_PROTECTION_BED_HYSTERESIS     5 // Degrees Celsius
 
   /**
    * As described above, except for the bed (M140/M190/M303).
    */
   #define WATCH_BED_TEMP_PERIOD                60 // Seconds
-  #define WATCH_BED_TEMP_INCREASE               2 // Degrees Celsius
+  #define WATCH_BED_TEMP_INCREASE               5 // Degrees Celsius
 #endif
 
 /**
